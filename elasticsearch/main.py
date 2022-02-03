41519ec408a1f309b770b6ba99681d22f2715db9
tanggrace <tanggrace.11@gmail.com>
2022-02-03 17:25:17 -0500

    Creates ES connection, creates ES index, creates document, very basic query

--- elasticsearch/main.py
+++ elasticsearch/main.py
@@ -1,60 +1,47 @@
 import json
+import logging
 from elasticsearch import Elasticsearch
 
 def connect_elasticsearch():
-    _es = None
     _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    if _es.ping():
-        print('Connected!')
-    else:
-        print('Could not connect')
+    logging.info(_es.ping())
     return _es
 
-def create_index(es_object, index_name='songs'):
-    created = False
-    # index settings
-    mappings = {
-        "mappings": {
-            "members": {
-                "dynamic": "strict",    # force Elasticsearch to do a strict checking of any incoming document
-                "properties": {
-                    "name": {
-                        "type": "text"
-                    },
-                    "artist": {
-                        "type": "text"
-                    },
-                    "album": {
-                        "type": "text"
-                    },
-                }
-            }
-        }
-    }
-    try:
-        if not es_object.indices.exists(index=index_name):
-            es_object.indices.create(index=index_name, mappings=mappings)
-            print('Created Index')
-            created = True
-    except Exception as ex:
-        print(str(ex))
-    finally:
-        return created
+def create_index(es_object, index_name, mapping):
+    logging.info(f'Creating index {index_name} with the following schema:\n{json.dumps(mapping, indent=2)}')
+    es_object.indices.create(index=index_name, ignore=400, mappings=mapping)
     
-def store_record(es_object, index_name, record):
-    try:
-        outcome = es_object.index(index=index_name, doc_type='_doc', document=record)
-    except Exception as ex:
-        print('Error in indexing data')
-        print(str(ex))
+def store_record(es_object, index_name, doc):
+    logging.info(f'Writing document to ES index {index_name}')
+    res = es_object.index(index=index_name, document=json.dumps(doc))
 
-def search(es_object, index_name, search):
-    res = es_object.search(index=index_name, body=search)
+def search(es_object, index_name, query):
+    res = es_object.search(index=index_name, body=json.dumps(query))
+    return res
+
+logging.basicConfig(level=logging.INFO)
 
 es = connect_elasticsearch()
-if es is not None:
-    create_index(es)
-    record_object = {'name': 'Blank Space', 'artist': 'Taylor Swift', 'album': '1989'}
-    store_record(es, 'songs', record_object)
-    search_object = {'query': {'match': {'artist': 'Taylor Swift'}}}
-    search(es, 'songs', json.dumps(search_object))
+song_mapping = {
+    "mappings": {
+        "properties": {
+            "name": {
+                "type": "text"
+            },
+            "artist": {
+                "type": "text"
+            },
+            "album": {
+                "type": "text"
+            },
+        }
+    }
+}
+create_index(es, 'songs', song_mapping)
+#print(json.dumps(es.indices.get_mapping(index='songs'), indent=1))
+record_object = {'name': 'Blank Space', 'artist': 'Taylor Swift', 'album': '1989'}
+store_record(es, 'songs', record_object)
+#print(es.count(index='songs'))
+search_object = {'query': {'match': {'artist': {'query': 'Taylor Swift'}}}}
+results = search(es, 'songs', search_object)
+print(results['hits']['hits'])
