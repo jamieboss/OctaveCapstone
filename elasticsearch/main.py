574db0ace04a65f7a5e3c50e0793696d5062a591
tanggrace <tanggrace.11@gmail.com>
2022-02-06 20:29:16 -0500

    Adds delete_index function

--- elasticsearch/main.py
+++ elasticsearch/main.py
@@ -9,8 +9,13 @@ def connect_elasticsearch():
 
 def create_index(es_object, index_name, mapping):
     logging.info(f'Creating index {index_name} with the following schema:\n{json.dumps(mapping, indent=2)}')
-    es_object.indices.create(index=index_name, ignore=400, mappings=mapping)
+    # ignore 400 cause by indexAlreadyExistsException when creating an index
+    es_object.indices.create(index=index_name, ignore=400, body=mapping)
     
+def delete_index(es_object, index_name):
+    logging.info(f'Deleting index {index_name}')
+    es_object.indices.delete(index=index_name, ignore=[400,404])
+
 def store_record(es_object, index_name, doc):
     logging.info(f'Writing document to ES index {index_name}')
     res = es_object.index(index=index_name, document=json.dumps(doc))
@@ -22,6 +27,7 @@ def search(es_object, index_name, query):
 logging.basicConfig(level=logging.INFO)
 
 es = connect_elasticsearch()
+delete_index(es, 'songs')
 song_mapping = {
     "mappings": {
         "properties": {
@@ -42,6 +48,7 @@ create_index(es, 'songs', song_mapping)
 record_object = {'name': 'Blank Space', 'artist': 'Taylor Swift', 'album': '1989'}
 store_record(es, 'songs', record_object)
 #print(es.count(index='songs'))
+# TODO: Need to add a refresh before searching
 search_object = {'query': {'match': {'artist': {'query': 'Taylor Swift'}}}}
 results = search(es, 'songs', search_object)
 print(results['hits']['hits'])
