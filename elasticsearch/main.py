ae344d9b015a84cac475fafbc22280a41d086575
Kevin Higgins <higgins.478@osu.edu>
2022-02-06 21:11:03 -0500

    Added method for retrieving properties of records in a search (see google doc) Played around with elasticsearch in python.

--- elasticsearch/main.py
+++ elasticsearch/main.py
@@ -18,14 +18,19 @@ def delete_index(es_object, index_name):
 
 def store_record(es_object, index_name, doc):
     logging.info(f'Writing document to ES index {index_name}')
-    res = es_object.index(index=index_name, document=json.dumps(doc))
+    res = es_object.index(index=index_name, document=json.dumps(doc), refresh = True)
 
 def search(es_object, index_name, query):
     res = es_object.search(index=index_name, body=json.dumps(query))
     return res
 
+def delete_index(es_object, index_name):
+    logging.info(f'Deleting index {index_name}.')
+    es_object.indices.delete(index_name, ignore=[400, 404])
+
 logging.basicConfig(level=logging.INFO)
 
+# Connecting to localhost es and creating mapping
 es = connect_elasticsearch()
 delete_index(es, 'songs')
 song_mapping = {
@@ -43,12 +48,19 @@ song_mapping = {
         }
     }
 }
+# Creating Index and Record
 create_index(es, 'songs', song_mapping)
-#print(json.dumps(es.indices.get_mapping(index='songs'), indent=1))
+print(json.dumps(es.indices.get_mapping(index='songs'), indent=1))
 record_object = {'name': 'Blank Space', 'artist': 'Taylor Swift', 'album': '1989'}
 store_record(es, 'songs', record_object)
-#print(es.count(index='songs'))
-# TODO: Need to add a refresh before searching
+print(es.count(index='songs'))
+
+# Searching for records
 search_object = {'query': {'match': {'artist': {'query': 'Taylor Swift'}}}}
 results = search(es, 'songs', search_object)
-print(results['hits']['hits'])
+for i in range(len(results['hits']['hits'])):
+    print(results['hits']['hits'][i]['_source']['name'])
+
+# Deleting index
+delete_index(es, 'songs')
+
