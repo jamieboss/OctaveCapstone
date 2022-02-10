497250a7a242727723240471f58212a207bd1e65
Kevin Higgins <higgins.478@osu.edu>
2022-02-10 15:16:02 -0500

    Added song mapping json file for format of songs in elasticsearch database as single point of change. Added elasticsearch class to be used as object when populating database. Added python script which will be used to load the song data into elasticsearch.

---
+++ elasticsearch/elasticsearch_db.py
@@ -0,0 +1,32 @@
+import os
+import json
+import logging
+from elasticsearch import Elasticsearch
+
+class ElasticsearchDB:
+
+    def __init__(self):
+        self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}]) # Change once we have shared elasticsearch
+        logging.info(self.es.ping())
+
+    def create_index(self, index_name, mapping):
+        logging.info(f'Creating index {index_name} with the following schema:\n{json.dumps(mapping, indent=2)}')
+        # ignore 400 cause by indexAlreadyExistsException when creating an index
+        self.es.indices.create(index=index_name, ignore=400, body=mapping)
+        
+    def delete_index(self, index_name):
+        logging.info(f'Deleting index {index_name}')
+        self.es.indices.delete(index=index_name, ignore=[400,404])
+
+    def store_record(self, index_name, doc):
+        logging.info(f'Writing document to ES index {index_name}')
+        res = self.es.index(index=index_name, document=json.dumps(doc), refresh = True)
+
+    def search(self, index_name, query):
+        res = self.es.search(index=index_name, body=json.dumps(query))
+        return res
+
+    def delete_index(self, index_name):
+        logging.info(f'Deleting index {index_name}.')
+        self.es.indices.delete(index_name, ignore=[400, 404])
+
