23da70a4a63e70e1e17f6bd165146108004085ea
dakotagetty <dakotagetty@gmail.com>
2022-02-15 15:00:14 -0500

    Changes to scratch. Needs to be updated more but routes.py has the most important information

--- octave-app/backend/features.py
+++ octave-app/backend/features.py
@@ -1,49 +1,110 @@
+from cgitb import reset
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 # Selected features that a song is given via Spotify API
 SONG_FEATURES = ["acousticness", "danceability", "energy", "speechiness", "tempo", "valence"]
 
 # All options for the action choice and its associated song features.
 action_features = {
-        'WORKINGOUT': {'acousticness': 0.125, 'danceability': 0.75, 'energy': 1, 'speechiness': 0.125, 'tempo': 135, 'valence': 0.5}, 
-        'STUDYING': {'acousticness': 0.5, 'danceability': 0.125, 'energy': 0.125, 'speechiness': 0.33, 'tempo': 95, 'valence': 0.66}, 
-        'DRIVING': {'acousticness': 0.125, 'danceability': 0.5, 'energy': 0.5, 'speechiness': 0.25, 'tempo': 125, 'valence': 0.5}, 
-        'DANCING': {'acousticness': 0.25, 'danceability': 1, 'energy': 0.5, 'speechiness': 0.25, 'tempo': 115, 'valence': 0.75}, 
-        'RELAXING': {'acousticness': 0.75, 'danceability': 0, 'energy': 0, 'speechiness': 0.5, 'tempo': 80, 'valence': 1},
-        }
+    'WORKINGOUT': {'acousticness': 0.125, 'danceability': 0.75, 'energy': 1, 'speechiness': 0.125, 'tempo': 135, 'valence': 0.5}, 
+    'STUDYING': {'acousticness': 0.5, 'danceability': 0.125, 'energy': 0.125, 'speechiness': 0.33, 'tempo': 95, 'valence': 0.66}, 
+    'DRIVING': {'acousticness': 0.125, 'danceability': 0.5, 'energy': 0.5, 'speechiness': 0.25, 'tempo': 125, 'valence': 0.5}, 
+    'DANCING': {'acousticness': 0.25, 'danceability': 1, 'energy': 0.5, 'speechiness': 0.25, 'tempo': 115, 'valence': 0.75}, 
+    'RELAXING': {'acousticness': 0.75, 'danceability': 0, 'energy': 0, 'speechiness': 0.5, 'tempo': 80, 'valence': 1},
+}
 
 # All options for the mood choice and its associated song features.
 mood_features = {
-        'ANGRY': {'acousticness': 0, 'danceability': 0.5, 'energy': 0.875, 'speechiness': 0.125, 'tempo': 150, 'valence': 0.125}, 
-        'ANXIOUS': {'acousticness': 0.5, 'danceability': 0.33, 'energy': 0.125, 'speechiness': 0, 'tempo': 75, 'valence': 1}, 
-        'ENERGIZED': {'acousticness': 0.125, 'danceability': 1, 'energy': 1, 'speechiness': 0.125, 'tempo': 145, 'valence': 0.5}, 
-        'JOYFULL': {'acousticness': 0.33, 'danceability': 0.25, 'energy': 0.33, 'speechiness': 0.5, 'tempo': 115, 'valence': 0.75}, 
-        'SAD': {'acousticness': 0.5, 'danceability': 0.125, 'energy': 0.125, 'speechiness': 0.25, 'tempo': 80, 'valence': 0}, 
-        'PEACEFUL': {'acousticness': 1, 'danceability': 0.125, 'energy': 0, 'speechiness': 0.66, 'tempo': 60, 'valence': 0.66}, 
-        'TIRED': {'acousticness': 0.66, 'danceability': 0, 'energy': 0.25, 'speechiness': 0.5, 'tempo': 85, 'valence': 0.75}
-        }
+    'ANGRY': {'acousticness': 0, 'danceability': 0.5, 'energy': 0.875, 'speechiness': 0.125, 'tempo': 150, 'valence': 0.125}, 
+    'ANXIOUS': {'acousticness': 0.5, 'danceability': 0.33, 'energy': 0.125, 'speechiness': 0, 'tempo': 75, 'valence': 1}, 
+    'ENERGIZED': {'acousticness': 0.125, 'danceability': 1, 'energy': 1, 'speechiness': 0.125, 'tempo': 145, 'valence': 0.5},       'JOYFULL': {'acousticness': 0.33, 'danceability': 0.25, 'energy': 0.33, 'speechiness': 0.5, 'tempo': 115, 'valence': 0.75}, 
+    'SAD': {'acousticness': 0.5, 'danceability': 0.125, 'energy': 0.125, 'speechiness': 0.25, 'tempo': 80, 'valence': 0}, 
+    'PEACEFUL': {'acousticness': 1, 'danceability': 0.125, 'energy': 0, 'speechiness': 0.66, 'tempo': 60, 'valence': 0.66}, 
+    'TIRED': {'acousticness': 0.66, 'danceability': 0, 'energy': 0.25, 'speechiness': 0.5, 'tempo': 85, 'valence': 0.75}
+}
+
+genre_features = {
+
+}
 
 
-def audio_features(action, mood):
-    """
-    Returns a dict of features with calculated values for each song feature based on provided action and mood.
+def select_audio_features(input):
 
-    :param action: str action type selection (i.e. WORKINGOUT, STUDYING)
-    :param mood: str mood type selection (i.e. ANGRY, ANXIOUS)
-    :return: a dict of features
-    """
+    # authorization information
+    CLIENT_ID = 'ecca6b6968774a20bc255c94ded4fbb4'
+    CLIENT_SECRET = 'ccb019ba7bd9409f93f2527ea42f0e88'
+    REDIRECT_URI = 'http://localhost'
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    song_features = {}
+    for song in input["favSongs"]:
+        # TODO? CAN WE USE ELASTICSEARCH HERE TO SEARCH FOR SONG AND ADD IT IF IT DOESNT EXIST?
+        # i.e
+        # if song in ELASTICSEARCH DB:
+            #raw_features = ELASTICSEARCH.GET(song)
+        # else: 
+            #res = sp.search(q=song, limit=1, offset=0, type="track", market="US")
+            #song_id = res["tracks"]["items"][0]["id"]
+            #raw_features = sp.audio_features(song_id)
+        res = sp.search(q=song, limit=1, offset=0, type="track", market="US")
+        song_id = res["tracks"]["items"][0]["id"]
+        raw_features = sp.audio_features(song_id)
+        song_features[song] = cleanse_features(raw_features[0])
+
+    artist_features = {}
+    for artist in input["favArtists"]:
+        res = sp.search(q=artist, limit=1, offset=0, type="artist", market="US")
+        # get audio feature of artist
+        artist_features[artist] = res["artists"]["items"][0]["id"]
+
+    genre_features = {}
+    for genre in input["favGenres"]:
+        # get audio feature of genre
+        genre_features[genre] = ""
+
     features = {}
+    action_feature = action_features[input["activities"][0]]
+    mood_feature = mood_features[input["mood"]]
     for feature in SONG_FEATURES:
-        features[feature] = set_feature(action, mood, feature)
-    return features
+        features[feature] = join_instance_features(action_feature, mood_feature, feature)
+    return song_features
 
-def set_feature(action, mood, feature):
-    """
-    Helper function of audio_features to calculate the song features for a choice of action and mood. 
-    Current algorithm is to return the average of the two values.
+def cleanse_features(features):
+    clean_features = {}
+    for feature in features:
+        if feature in SONG_FEATURES:
+            clean_features[feature] = features[feature]
+    return clean_features
 
-    :param action: str action type selection (i.e. WORKINGOUT, STUDYING)
-    :param mood: str mood type selection (i.e. ANGRY, ANXIOUS)
-    :param feature: str feature from SONG_FEATURES (i.e. acousticness, danceability)
-    :return: a float value of the assigned feature
-    """
-    res = round(action_features[action][feature] + mood_features[mood][feature] / 2, 5)
+def join_instance_features(action_features, mood_features, feature):
+    action_feature = action_features[feature]
+    mood_feature = mood_features[feature]
+    songs_features = {}
+
+    res = round(action_feature + mood_feature / 2, 5)
     return res        
+
+sample_input = {
+    "INPUT":
+        {
+            "activities": ["RELAXING"],
+            "favArtists": ["Avicii","Martin Garrix","Kanye West"],
+            "favGenres": ["EDM","Rap","Pop"],
+            "mood": "TIRED",
+            "num_songs": 50,
+            "favSongs": ["Waiting for Love","High on Life","Homecoming"]
+        },
+    "OUTPUT":
+        {
+            "acousticness":1.08, 
+            "danceability":0.0, 
+            "energy":0.125, 
+            "speechiness":0.75, 
+            "tempo":122.5, 
+            "valence":1.375
+        }
+    }
+select_audio_features(sample_input["INPUT"])
--- octave-app/backend/spotify-scripts/scripts/test.py
+++ octave-app/backend/spotify-scripts/scripts/test.py
@@ -1,32 +1,87 @@
+import random
+import time
+import numpy as np
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # authorization information
-CLIENT_ID = 'f54a10c8ab084526a1df90b9c7f4f19f'
-CLIENT_SECRET = '2178e29186714432b61020743e950821'
-REDIRECT_URI = 'http://localhost/'
+CLIENT_ID = 'ecca6b6968774a20bc255c94ded4fbb4'
+CLIENT_SECRET = 'ccb019ba7bd9409f93f2527ea42f0e88'
+REDIRECT_URI = 'http://localhost'
 
-scope = 'user-library-read'
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope))
+NUM_INIT_ARTISTS = 5
+NUM_ALBUMS_ARTIST = 2
+NUM_TRACKS_ALBUM = 4
+
+
+# function for getting related artists
+def getRelatedArtists(artistId, depth=1, numArtists=3):
+    artists = sp.artist_related_artists(artistId)['artists']
+    if len(artists) > numArtists:
+        artists = random.sample(artists, numArtists)
+    # recursively call the getRelatedArtists function to get artists related to the related artist
+    if depth > 1:
+        artistLayer = [artist for artist in artists]
+        for artist in artists:
+            artists = getRelatedArtists(artist['id'], depth-1, 2)
+            artistLayer = artistLayer + artists
+        artists = artistLayer
+    return artists
+
+# function to get random tracks per album
+def getRandomTracks(tracks):
+    randTracks = []
+    if len(tracks) > NUM_TRACKS_ALBUM:
+        randTracks = random.sample([track['id'] for track in tracks], NUM_TRACKS_ALBUM)
+    else:
+        randTracks = [track['id'] for track in tracks]
+    return randTracks
 
 # this is where we will take input from the web app for liked artists to find similar ones
-steve_lacy = "spotify:artist:57vWImR43h4CaDao012Ofp"
-artists = sp.artist_related_artists(steve_lacy)['artists']
+steveLacy = "spotify:artist:57vWImR43h4CaDao012Ofp"
+# random depth search for related artists to possibly find unlistened to music
+mostSimilarArtists = getRelatedArtists(steveLacy, 0, NUM_INIT_ARTISTS)
+# create a depth map for how deep we will search for each artist
+artistDepths = [random.randint(0,2) for i in range(len(mostSimilarArtists))]
+# list of ids for all of the related artists
+relatedArtistIds = [artist['id'] for artist in mostSimilarArtists]
 
-# loops over all related artists
-for artist in artists:
-    print(artist['name'])
+# loop over most similar artists and continue adding related artists depending on the depth map
+for i, artist in enumerate(mostSimilarArtists):
+    if artistDepths[i] >= 1:
+        artists = getRelatedArtists(artist['id'], artistDepths[i])
+        for a in artists:
+            if a['id'] not in relatedArtistIds:
+                relatedArtistIds.append(a['id'])
+    
+relatedArtistIds = random.sample(relatedArtistIds, 5)
+
+preclassifiedTracks = []
+for artistId in relatedArtistIds:
     # get all of the artists albums
-    albums = sp.artist_albums(artist['id'])['items']
+    artistAlbums = sp.artist_albums(artistId)['items']
     # store the ids of the albumns to do create an efficient request
-    album_ids = []
-    for album in albums:
-        album_ids.append(album['id'])
+    albumIds = []
+    if len(artistAlbums) > NUM_ALBUMS_ARTIST:
+        albumIds = random.sample([album['id'] for album in artistAlbums], NUM_ALBUMS_ARTIST)
+    else:
+        albumIds = [album['id'] for album in artistAlbums]
     # gets the tracks from the returned albums
-    track_list = sp.albums(album_ids)['albums']
-    for i in range(2):
-        tracks = track_list[i]['tracks']['items']
-        for j in range(1):
-            print(tracks[j]['name'])
-    print()
+    albums = sp.albums(albumIds)['albums']
+    # list of tracks to be used in the mood classification
+
+    if len(albums) >= NUM_ALBUMS_ARTIST:
+        for i in range(NUM_ALBUMS_ARTIST):
+            tracks = albums[i]['tracks']['items']
+            preclassifiedTracks = preclassifiedTracks + getRandomTracks(tracks)
+    else:
+        print("Only one album :(")
+        tracks = albums['tracks']['items']
+        preclassifiedTracks = preclassifiedTracks + getRandomTracks(tracks)
+
+# makes sure all tracks in the list are unique
+preclassifiedTracks = np.unique(np.array(preclassifiedTracks))
+print(preclassifiedTracks)
