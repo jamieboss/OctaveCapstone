3040478ed2ba0c76b437b82363a4defad97dca62
L0gan012 <logan.sauer.012@gmail.com>
2022-02-16 14:14:43 -0500

    Refactored OAuth and added the csv artist file

--- spotify-scripts/SongFeatures.py
+++ spotify-scripts/SongFeatures.py
@@ -1,19 +1,9 @@
-import spotipy
 import numpy as np
 import pandas as pd
-
-
-from spotipy.oauth2 import SpotifyOAuth
-
-CLIENT_ID = 'f54a10c8ab084526a1df90b9c7f4f19f'
-CLIENT_SECRET = '2178e29186714432b61020743e950821'
-REDIRECT_URI = 'http://localhost/'
-scope = 'user-library-read'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope))
-
+from SpotifyOAuth import sp
 
 #read from database
-df = pd.read_csv("artist-uris.csv.csv")
+df = pd.read_csv('../artist-uris.csv')
 df.head
 #gets list of all spotify artist ids,
 #IDs = df.iloc[:,1]
