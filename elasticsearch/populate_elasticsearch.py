c5235697602ca7994636c8fd2890fc415e73a36b
tanggrace <tanggrace.11@gmail.com>
2022-03-01 14:25:51 -0500

    Updates song_mapping, Moves elasticsearch tests to separate folder

---
+++ elasticsearch/populate_elasticsearch.py
@@ -0,0 +1,25 @@
+
+from turtle import end_fill
+import elasticsearch_db as esd
+import json
+
+# Create the elasticsearch_db object
+es_db = esd.ElasticsearchDB()
+
+# Create song index
+with open('elasticsearch/song_mapping.json') as f: # Load the song mapping json from this directory.
+    song_mapping = json.load(f)
+es_db.create_index('songs', song_mapping)
+
+# Get songList from external script
+songList = []
+
+# Store all songs from songList into elasticsearch
+for song in songList:
+    es_db.store_record('songs', song)
+
+# Test to see if all songs are returned when queried
+search_object = {'query': {'match_all': {}}}
+results = es_db.search('tests', search_object)
+for i, doc in enumerate(results['hits']['hits']):
+    print(f'Hit {i+1}: {doc["_source"]["name"]}')
