51b009209a395d91c2ab9bf29e25f3a3b939373b
UberFood <59709698+UberFood@users.noreply.github.com>
2022-03-01 14:34:57 -0500

    Update playlist generation
    Sends 3 songs related to user input (spotify recommendation under the hood + our moods/activities target values)

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,68 @@
+<<<<<<< Updated upstream
 from flask import Flask, redirect
 
 app = Flask(__name__)
+=======
+from flask import Flask, redirect, jsonify, request, session
+from flask_cors import CORS, cross_origin
+import requests
+from SpotifyOAuth import sp
+from song_search_examples import favSongs
+from features_alter import select_features_alter
+from playlist_generation import playlist_from_input
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SECRET_KEY'] = 'osd(99092=36&462134kjKDhuIS_d23'
+
+@app.route('/data', methods=['POST'])
+@cross_origin()
+def user_query():
+    print(request.get_json())
+    req = request.get_json()
+
+    '''
+    Example: 
+    This loop below iterates over each favorite song and returns its track id, name, and artist
+    My spotify API is not working so these "results" are pulled from three manual hits to the API i made to get song data and I am treating it like a black box currently. 
+    '''
+    
+    #input_features = select_features_alter(req);
+    #print(input_features);
+    playlist = playlist_from_input(req);
+
+    song_results = {}
+
+    #print(playlist['tracks'][0]['external_urls']);
+
+    for i in range(3):
+
+    	track_id = playlist['tracks'][i]['id']
+    	song_results[track_id] = [];
+    	song_results[track_id].append(playlist['tracks'][i]['name']);
+    	song_results[track_id].append(playlist['tracks'][i]['artists'][0]['name']);
+    	song_results[track_id].append(playlist['tracks'][i]['external_urls']['spotify']);
+
+
+    #TODO Replace above example with desired analysis to gather playlist
+
+    '''
+    Important! This is how song_results should be formatted in order for the front end to parse it correctly.
+    song_results = {track_id_1: [song_name, song_artist, spotify_link], ... , track_id_n: [song_name, song_artist, spotify_link]}
+    '''
+    return jsonify(result=song_results)
+
+@app.route('/playlist', methods=['POST'])
+@cross_origin()
+def playlist_query():
+    print(request.get_json())
+    req = request.get_json()
+
+    
+    playlistLink = "https://open.spotify.com/playlist/068wH7INPasnsH5HL8wBok"
+    return jsonify(message=playlistLink)
+>>>>>>> Stashed changes
 
 import routes
---
+++ flask/playlist_generation.py
@@ -0,0 +1,32 @@
+from cgitb import reset
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from features_alter import select_features_alter
+
+def playlist_from_input(input):
+    # authorization information
+
+    CLIENT_ID = 'f54a10c8ab084526a1df90b9c7f4f19f'
+    CLIENT_SECRET = '2178e29186714432b61020743e950821'
+    REDIRECT_URI = 'http://localhost'
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    input_features = select_features_alter(input);
+
+    input_artists_id = [];
+    for (artist) in input['favArtists']:
+    	res = sp.search(q=artist, limit=1, offset=0, type="artist", market="US");
+    	artist_id = res["artists"]["items"][0]["id"];
+    	input_artists_id.append(artist_id);
+
+    input_songs_id = [];
+    for (song) in input['favSongs']:
+    	res = sp.search(q=song, limit=3, offset=0, type="track", market="US");
+    	song_id = res["tracks"]["items"][0]["id"];
+    	input_songs_id.append(song_id);
+    
+    songs = sp.recommendations(seed_artists = input_artists_id, seed_genres=None, seed_tracks=input_songs_id, limit=10, country=None, target_acousticness = input_features[0], target_danceability = input_features[1], target_energy = input_features[2], target_speechiness = input_features[3], target_tempo = input_features[4], target_valence = input_features[5]);
+
+    return songs;
