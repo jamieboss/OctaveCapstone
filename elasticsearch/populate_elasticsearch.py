4646756be883ff6d8e565cde25a53dba13c2190e
tanggrace <tanggrace.11@gmail.com>
2022-03-03 14:30:20 -0500

    Fixes import of song_mapping file

--- elasticsearch/populate_elasticsearch.py
+++ elasticsearch/populate_elasticsearch.py
@@ -11,13 +11,13 @@ import SongFeatures
 es_db = ElasticsearchDB()
 
 # Create song index
-with open('elasticsearch/song_mapping.json') as f: # Load the song mapping json from this directory.
+mapping_file = os.path.dirname(os.path.realpath(__file__)) + '\song_mapping.json'
+with open(mapping_file) as f: # Load the song mapping json from this directory.
     song_mapping = json.load(f)
 es_db.create_index('songs', song_mapping)
 
 # Get songList from external script
 songList = SongFeatures.get_tracks_for_es(1)
-print(songList)
 
 # Store all songs from songList into elasticsearch
 for i, song in enumerate(songList):
@@ -25,6 +25,6 @@ for i, song in enumerate(songList):
 
 # Test to see if all songs are returned when queried
 search_object = {'query': {'match_all': {}}}
-results = es_db.search('tests', search_object)
+results = es_db.search('songs', search_object)
 for i, doc in enumerate(results['hits']['hits']):
     print(f'Hit {i+1}: {doc["_source"]["name"]}')
